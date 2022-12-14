1c8c2fbcffcc3860586c0d1206e5f7e08d3bcfd2
Jelle <jellehak@gmail.com>
2022-12-14 17:19:35 +0100

    changes

--- components/index.js
+++ components/index.js
@@ -5,14 +5,24 @@
         <v-icon class="mdi-label mdi v-icon"></v-icon>
  */
 class VIcon extends HTMLElement {
+    static get observedAttributes() {
+        return ["icon"];
+    }
+    attributeChangedCallback(key = "", oldValue, newValue) {
+        console.log(key, newValue)
+        this.classList.remove(oldValue)
+        this.classList.add(newValue)
+    }
     connectedCallback() {
         this.classList.add('mdi')
         this.classList.add('v-icon')
-        if(!this.getAttribute('icon')) {
-            console.warn('Missing icon attribute', this)
+
+        const icon = this.getAttribute('icon')
+        if (!icon) {
+            console.warn('Missing icon', this)
             return
         }
-        this.classList.add(this.getAttribute('icon') || '')
+        this.classList.add(icon)
     }
 }
 
@@ -30,25 +40,24 @@ customElements.define('v-chip', VChip);
 
 class VBtn extends HTMLElement {
     connectedCallback() {
-        {
-            const el = document.createElement('span')
-            el.classList.add('v-btn__content')
-            el.innerHTML = this.innerHTML
-            this.innerText = ''
-            this.append(el)
-        }
+        // {
+        //     const el = document.createElement('span')
+        //     el.classList.add('v-btn__content')
+        //     el.innerHTML = this.innerHTML
+        //     this.innerText = ''
+        //     this.append(el)
+        // }
         {
             const el = document.createElement('span')
             el.classList.add('v-btn__overlay')
             this.prepend(el)
         }
-
         {
             const el = document.createElement('span')
             el.classList.add('v-btn__underlay')
             this.prepend(el)
         }
-        
+
     }
 }
 
@@ -63,3 +72,36 @@ class VSheet extends HTMLElement {
 }
 
 customElements.define('v-sheet', VSheet);
+
+class VCard extends HTMLElement {
+    connectedCallback() {
+        const el = document.createElement('span')
+        el.classList.add('v-card__underlay')
+        this.append(el)
+    }
+}
+
+customElements.define('v-card', VCard);
+
+class VImg extends HTMLElement {
+    connectedCallback() {
+        const el = document.createElement('img')
+        el.src = this.getAttribute('src')
+        this.append(el)
+    }
+}
+
+customElements.define('v-img', VImg);
+
+class VExpandTransition extends HTMLElement {
+    connectedCallback() {
+
+    }
+    toggle() {
+        const el = this
+        el.classList.toggle('expanded')
+        el.classList.toggle('collapsed')
+    }
+}
+
+customElements.define('v-expand-transition', VExpandTransition);
